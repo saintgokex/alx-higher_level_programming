a30c765895a4a3f03fbfe2b5328e2e01895f2b45
saintgokex <drgokex@gmail.com>
2022-11-29 00:12:30 +0100

    spaces fixed

--- 0x00-python-hello_world/testing.c
+++ 0x00-python-hello_world/testing.c
@@ -24,7 +24,7 @@ int main(void)
 	add_nodeint(&head, 4);
 	add_nodeint(&head, 98);
 	add_nodeint(&head, 402);
-        add_nodeint(&head, 1024);
+	add_nodeint(&head, 1024);
 	print_listint(head);
 
 	if (check_cycle(head) == 0)
@@ -42,14 +42,10 @@ int main(void)
 		printf("Linked list has no cycle\n");
 	else if (check_cycle(head) == 1)
 		printf("Linked list has a cycle\n");
-
 	current = head;
 	for (i = 0; i < 4; i++)
 		current = current->next;
 	current->next = temp;
-
 	free_listint(head);
-
 	return (0);
-
 }
